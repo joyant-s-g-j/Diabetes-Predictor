7c3f8d95a3c13041ee68c74c0136f34b9a06f3f7
joyantgupta <joyantsheikharguptajoy@gmail.com>
2026-01-17 07:07:45 +0600

    split

--- rf_train.py
+++ rf_train.py
@@ -76,3 +76,7 @@ pipeline = Pipeline(steps=[
     ('preprocessor', preprocessor),
     ('model', model)
 ])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
